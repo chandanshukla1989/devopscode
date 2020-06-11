293c073bb4a2b0d5f15e7c24e7aa771d624177d5
chandanshukla1989 <50393216+chandanshukla1989@users.noreply.github.com>
2020-06-12 04:56:18 +0800

    Update main.sh

--- code_backup/main.sh
+++ code_backup/main.sh
@@ -1,28 +1,28 @@
 terraform init
 gcloud -q compute images delete adhocvm-image --project=imperial-legacy-232115 
 gcloud -q compute instances delete adhoc-vm --project=imperial-legacy-232115 --zone=us-west1-a
-> ./terraform.tfstate
+> /home/shukla_chandan64/gitpush/terraform.tfstate
 terraform apply -auto-approve
 
 #########################prepare host for /etc/hosts file
 cat terraform.tfstate|grep network_ip|cut -d'"' -f4|awk '{print $1" adhoc-vm"}' > host_etc
 sudo bash -c 'cat host_etc > /etc/hosts'
 ssh-keygen -f "/home/shukla_chandan64/.ssh/known_hosts" -R adhoc-vm
-chmod 400 ./keyfiles/privatekey.pem
-ssh -o "StrictHostKeyChecking=no" -i ./keyfiles/privatekey.pem adhoc-vm "echo Adding Keys in Host"
+chmod 400 /home/shukla_chandan64/gitpush/keyfiles/privatekey.pem
+ssh -o "StrictHostKeyChecking=no" -i /home/shukla_chandan64/gitpush/keyfiles/privatekey.pem adhoc-vm "echo Adding Keys in Host"
 
 #########################prepare hostlist for ansible
-echo -e "[adhoc-vm]\nadhoc-vm ansible_ssh_private_key_file=./keyfiles/privatekey.pem\n[runner-vm]\nrunner-vm ansible_ssh_private_key_file=./keyfiles/privatekey.pem" > ./ansible_playbooks/ansible_host
+echo -e "[adhoc-vm]\nadhoc-vm ansible_ssh_private_key_file=/home/shukla_chandan64/gitpush/keyfiles/privatekey.pem\n[runner-vm]\nrunner-vm ansible_ssh_private_key_file=/home/shukla_chandan64/gitpush/keyfiles/privatekey.pem" > /home/shukla_chandan64/gitpush/ansible_playbooks/ansible_host
 cat /etc/hosts
-cat ./ansible_playbooks/ansible_host
+cat /home/shukla_chandan64/gitpush/ansible_playbooks/ansible_host
 sleep 10s
-ansible-playbook ./ansible_playbooks/pipinstall.yaml -i ./ansible_playbooks/ansible_host
-ansible-playbook ./ansible_playbooks/install-python-packages.yaml -i ./ansible_playbooks/ansible_host
+ansible-playbook /home/shukla_chandan64/gitpush/ansible_playbooks/pipinstall.yaml -i /home/shukla_chandan64/gitpush/ansible_playbooks/ansible_host
+ansible-playbook /home/shukla_chandan64/gitpush/ansible_playbooks/install-python-packages.yaml -i /home/shukla_chandan64/gitpush/ansible_playbooks/ansible_host
 gcloud compute instances stop adhoc-vm --zone=us-west1-a
 gcloud compute images create adhocvm-image --project=imperial-legacy-232115 --source-disk=adhoc-vm --source-disk-zone=us-west1-a
 > ./terraform.tfstate
 gcloud -q compute instances delete runner-vm --project=imperial-legacy-232115 --zone=us-west1-a
-terraform apply -auto-approve ./restore_from_image 
+terraform apply -auto-approve /home/shukla_chandan64/gitpush/restore_from_image 
 ssh-keygen -f "/home/shukla_chandan64/.ssh/known_hosts" -R runner-vm
 cat terraform.tfstate|grep network_ip|cut -d'"' -f4|awk '{print $1" runner-vm"}' > host_etc
 sudo bash -c 'cat host_etc >> /etc/hosts'
