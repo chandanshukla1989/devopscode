e140e042985adffd28cfc705480ccf3153c0b038
chandanshukla1989 <50393216+chandanshukla1989@users.noreply.github.com>
2020-06-12 04:45:05 +0800

    Update main.sh

--- code_backup/main.sh
+++ code_backup/main.sh
@@ -1,3 +1,4 @@
+terraform init
 gcloud -q compute images delete adhocvm-image --project=imperial-legacy-232115 
 gcloud -q compute instances delete adhoc-vm --project=imperial-legacy-232115 --zone=us-west1-a
 > ./terraform.tfstate
@@ -7,6 +8,7 @@ terraform apply -auto-approve
 cat terraform.tfstate|grep network_ip|cut -d'"' -f4|awk '{print $1" adhoc-vm"}' > host_etc
 sudo bash -c 'cat host_etc > /etc/hosts'
 ssh-keygen -f "/home/shukla_chandan64/.ssh/known_hosts" -R adhoc-vm
+chmod 400 ./keyfiles/privatekey.pem
 ssh -o "StrictHostKeyChecking=no" -i ./keyfiles/privatekey.pem adhoc-vm "echo Adding Keys in Host"
 
 #########################prepare hostlist for ansible
@@ -19,6 +21,7 @@ ansible-playbook ./ansible_playbooks/install-python-packages.yaml -i ./ansible_p
 gcloud compute instances stop adhoc-vm --zone=us-west1-a
 gcloud compute images create adhocvm-image --project=imperial-legacy-232115 --source-disk=adhoc-vm --source-disk-zone=us-west1-a
 > ./terraform.tfstate
+gcloud -q compute instances delete runner-vm --project=imperial-legacy-232115 --zone=us-west1-a
 terraform apply -auto-approve ./restore_from_image 
 ssh-keygen -f "/home/shukla_chandan64/.ssh/known_hosts" -R runner-vm
 cat terraform.tfstate|grep network_ip|cut -d'"' -f4|awk '{print $1" runner-vm"}' > host_etc
